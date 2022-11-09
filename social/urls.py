575af4c188729805fb0aa06c6e37901a9484fdee
Xoxxox21 <fauzan01062000@gmail.com>
2022-11-09 21:23:06 +0700

    membuat setting akun dari user

--- social/urls.py
+++ social/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', include('core.urls'))
 ]
 
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

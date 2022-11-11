2f74d9fadaed4417a7468cccdf2d0961f48931b2
Xoxxox21 <fauzan01062000@gmail.com>
2022-11-11 16:43:34 +0700

    membuat fitur search diindex

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('signup',views.signup, name='signup'),
     path('follow',views.follow, name='follow'),
+    path('search',views.search, name='search'),
     path('profile/<str:pk>',views.profile, name='profile'),
     path('like_post',views.like_post, name='like_post'),
     path('upload',views.upload, name='upload'),
